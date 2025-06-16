f792c102641dff5f1cbc5340136a73bf6543a47f
Moritz Béla Meier <moritz@moritxius.de>
2025-06-16 12:48:32 +0200

    Update _not-found.tsx

--- frontend/app/_not-found.tsx
+++ frontend/app/_not-found.tsx
@@ -1,4 +1,9 @@
-"use client"
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "404 - Not Found",
+  description: "The page you're looking for doesn't exist.",
+};
 
 export default function NotFound() {
   return (
